2ea73374fc5a836dc2419fd58b2f166821290ec1
Ahmed Skopljak <ahmed.skopljak.23@size.ba>
2026-01-06 01:59:31 +0100

    Dodane nove stvari, popravljanje dizajna

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -15,7 +15,7 @@ function Navigation({variant = "default"}) {
         <nav className='flex items-center py-6 px-16 justify-between gap-20 custom-nav'>
             <div className='flex items-center gap-6'>
                 {/* Logo */}
-                <a className='text-3xl text-black font-bold gap-8' href='/'>ShopEase</a>
+                <a className='text-3xl text-black font-bold gap-8' href='/'>Shopway</a>
             </div>
             { variant ==="default" &&
                 <div className='flex flex-wrap items-center gap-10'>
@@ -24,7 +24,7 @@ function Navigation({variant = "default"}) {
                         <li><NavLink to='/' className={({isActive})=> isActive ? 'active-link':''}>Shop</NavLink></li>
                         <li><NavLink to='/men' className={({isActive})=> isActive ? 'active-link':''}>Men</NavLink></li>
                         <li><NavLink to='/women' className={({isActive})=> isActive ? 'active-link':''}>Women</NavLink></li>
-                        <li><NavLink to='/kids' className={({isActive})=> isActive ? 'active-link':''}>Kids</NavLink></li>
+                        <li><NavLink to='/kids' className={({isActive})=> isActive ? 'active-link':''}>Children</NavLink></li>
                     </ul>
 
                 </div>
@@ -32,8 +32,8 @@ function Navigation({variant = "default"}) {
             { variant ==="default" &&
                 <div className='flex justify-center'>
                     {/* Search bar */}
-                    <div className='border rounded flex overflow-hidden'>
-                        <div className="flex items-center justify-center px-4 border-1">
+                    <div className='border border-gray-400 rounded flex overflow-hidden'>
+                        <div className="flex items-center justify-center px-4 border border-gray-400">
                             <svg className="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
                             <input type="text" className="px-4 py-2 outline-none" placeholder="Search"/>
                         </div>
